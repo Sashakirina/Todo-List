7dbdb28ef57c7a1ea596164ecb2da83afbeefdfc
Oleksandra <sashakirina@gmail.com>
2024-10-11 11:20:26 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+import './App.css'
+
+const App: FC = ()=> {
+
+
+  return (
+    <div className ="App">
+      <div></div>
+ 
+    </div>
+  )
+}
+
+export default App
